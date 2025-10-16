0d4e926006d07dac59d492a52234072faf623ddd
Tom Swain <tom@4eighty.com>
2025-10-16 10:37:05 -0700

    wip

--- app/page.jsx
+++ app/page.jsx
@@ -1,25 +1,23 @@
-'use client'
-import React from "react";
-import HeaderSlider from "@/components/HeaderSlider";
-import HomeProducts from "@/components/HomeProducts";
-import Banner from "@/components/Banner";
-import NewsLetter from "@/components/NewsLetter";
-import FeaturedProduct from "@/components/FeaturedProduct";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+'use client';
+import React from 'react';
+import HeroSection from '@/components/HeroSection';
+import HomeProducts from '@/components/HomeProducts';
+import Banner from '@/components/Banner';
+import NewsLetter from '@/components/NewsLetter';
+import FeaturedProduct from '@/components/FeaturedProduct';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 const Home = () => {
   return (
     <>
-      <Navbar/>
-      <div className="px-6 md:px-16 lg:px-32">
-        <HeaderSlider />
-        <HomeProducts />
-        <FeaturedProduct />
-        <Banner />
-        <NewsLetter />
-      </div>
-      <Footer />
+      <Navbar />
+      <HeroSection />
+      {/* <HomeProducts /> */}
+      {/* <FeaturedProduct /> */}
+      {/* <Banner /> */}
+      {/* <NewsLetter />  */}
+      {/* <Footer /> */}
     </>
   );
 };
